3a46dcad7380e7eff16213b3656f336bc24d59d0
Vlad <drwowwitcher@gmail.com>
2023-06-29 10:54:57 +0200

    Update Account.java

--- src/main/java/defusion/Account.java
+++ src/main/java/defusion/Account.java
@@ -4,12 +4,15 @@ import lombok.Data;
 
 @Data
 public class Account {
+
+    private static final TRANSACTION_SUM_DIV = 1000; 
+    
     private final Country country;
     private int balance = 0;
     private int transactionSum;
 
     void setTransactionSum() {
-        transactionSum = balance / 1000;
+        transactionSum = balance / TRANSACTION_SUM_DIV;
     }
 
     public boolean nonEmpty() {
